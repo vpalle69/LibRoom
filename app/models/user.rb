14e81062dec75f6026a9a69c9ed533a3dc0d6a83
vpalle69 <vpalle@ncsu.edu>
2016-09-25 20:45:38 -0400

    minor changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   validates :password, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,length:{maximum:255},format: {with: VALID_EMAIL_REGEX },uniqueness: { case_sensitive: false }
-  has_many :bookings, dependent: :destroy
+  has_many :bookings
 end
