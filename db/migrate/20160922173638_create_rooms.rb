0b674f0299b2e9dda85fc0378b67fba9c48bf7a4
vpalle69 <vpalle@ncsu.edu>
2016-10-05 09:07:37 -0400

    Minor db changes

--- db/migrate/20160922173638_create_rooms.rb
+++ db/migrate/20160922173638_create_rooms.rb
@@ -1,9 +1,9 @@
 class CreateRooms < ActiveRecord::Migration[5.0]
   def change
     create_table :rooms do |t|
-      t.integer :roomno
+      t.string :roomno
       t.string :building
-      t.integer :size
+      t.string :size
       t.string :status
 
       t.timestamps
--- db/migrate/20160922173702_create_bookings.rb
+++ db/migrate/20160922173702_create_bookings.rb
@@ -2,11 +2,12 @@ class CreateBookings < ActiveRecord::Migration[5.0]
   def change
     create_table :bookings do |t|
       t.belongs_to :room, index=true
-      t.integer :roomno
+      t.string :roomno
       t.string :booked_user
       t.datetime :starttime
       t.datetime :endtime
-
+      t.integer  :room_id
+      t.integer  :user_id
       t.timestamps
     end
   end
